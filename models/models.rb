a21eff6802fdc5ecfa65d3d5d46e47df50dc7282
Mr Rogers <jon@bunnymatic.com>
2012-03-02 20:27:38 -0800

    * move to sass  * restyle with color for baby

--- models/models.rb
+++ models/models.rb
@@ -48,6 +48,41 @@ class BabyImageResource
   end
 end
 
+class EventResource
+  include DataMapper::Resource
+  property :id, Serial
+  property :title, String, :length => 255
+  property :description, String, :length => 1024
+  property :address, String, :length => 255
+  property :starttime, DateTime
+  property :endtime, DateTime
+  property :url, String, :length => 255
+
+  validates_presence_of :starttime
+  validates_presence_of :title
+
+  def map_link
+    if address.present?
+      "http://maps.google.com/maps?q=%s" % URI.escape(address, /[[:punct:][:space:]]/)
+    end
+  end
+    
+  def trimmed_description
+    trimmed = []
+    words = description.split
+    nxt = 0
+    while trimmed.join.length < 100 && nxt < words.length
+      trimmed << words[nxt]
+      nxt += 1
+    end
+    result = trimmed.join ' '
+    if result.length < words.join.length
+      result << "..."
+    end
+    result
+  end
+end
+
 class Object
   def empty?
     (self == nil) || (self.respond_to?(:length) && self.length == 0)
--- romey.rb
+++ romey.rb
@@ -242,40 +242,6 @@ class Romey < Sinatra::Base
   end
 end
 
-class EventResource
-  include DataMapper::Resource
-  property :id, Serial
-  property :title, String, :length => 255
-  property :description, String, :length => 1024
-  property :address, String, :length => 255
-  property :starttime, DateTime
-  property :endtime, DateTime
-  property :url, String, :length => 255
-
-  validates_presence_of :starttime
-  validates_presence_of :title
-
-  def map_link
-    if address.present?
-      "http://maps.google.com/maps?q=%s" % URI.escape(address, /[[:punct:][:space:]]/)
-    end
-  end
-    
-  def trimmed_description
-    trimmed = []
-    words = description.split
-    nxt = 0
-    while trimmed.join.length < 100 && nxt < words.length
-      trimmed << words[nxt]
-      nxt += 1
-    end
-    result = trimmed.join ' '
-    if result.length < words.join.length
-      result << "..."
-    end
-    result
-  end
-end
 
 Dir[File.join(File.dirname(__FILE__),"models/**/*.rb")].each do |file|
   require file
